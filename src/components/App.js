c7faf334b0436b29d408c0033c4bfed39b169188
Anurag Dixit <34228896+AnDx25@users.noreply.github.com>
2021-03-14 13:05:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,29 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
+import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
-		this.state={
-			isVisible:false
-		}
-	};
-
-	 handleClick=()=>{
-		this.setState({
-      isVisible:true
-		})
+		this.state={display:false};
+		this.handleClick = this.handleClick.bind(this)
 	}
+
+handleClick(){
+ this.setState({display:true});
+
+ 
+}
+
     render() {
     	return(
     		<div id="main">
-				<button id="click" onClick={this.handleClick}>Show Text</button>
-				{
-					this.state.isVisible && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-				}
+				<button id='click' onClick={this.handleClick} >Click</button>
+{this.state.display===true?<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:''}
+				{ /* Do not remove this main div!! */ }
     		</div>
     	);
     }
 }
-
-
 
 
 export default App;
