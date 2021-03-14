64449181193b32f8110ce333057058f0ddefcdcb
AnDx25 <a.dixit@iiitmanipur.ac.in>
2021-03-14 12:03:12 +0530

    Show Text

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			isVisible:false
+		}
 	};
 
 	 handleClick=()=>{
-		
+		this.setState({
+      isVisible:true
+		})
 	}
     render() {
     	return(
     		<div id="main">
-				<button id="click" onClick={()=><p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-</p>}>Add Para</button>
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={this.handleClick}>Show Text</button>
+				{
+					this.state.isVisible?(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>):''
+				}
     		</div>
     	);
     }
 }
 
 
-export default App;
 
+
+export default App;
